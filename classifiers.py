952676a6a82cccbbd0d59700f8d8ff49363ddfcc
Ivan Prsa <prsa.ivan13@gmail.com>
2019-10-26 16:24:09 +0200

    implementing confusion matrix plot and other minor changes

--- classifiers.py
+++ classifiers.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
+
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
+from helpers import plot_confusion_matrix
+
 
 def svm_classifier(json_data):
-    sv_class = SVC(kernel='linear') # linear/poly/rbf/sigmoid
+    sv_class = SVC(kernel='poly', degree=2) # linear/poly/rbf/sigmoid
 
     df = pd.DataFrame(data=json_data)
 
@@ -22,9 +25,9 @@ def svm_classifier(json_data):
 
     y_pred = sv_class.predict(x_test)
 
-    print('SVM classifier:')
-    print(confusion_matrix(y_test, y_pred))
+    print('SVM classifier')
     print(classification_report(y_test, y_pred))
+    plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='SVM classifier')
 
 
 def decision_tree_classifier(json_data):
@@ -40,9 +43,9 @@ def decision_tree_classifier(json_data):
 
     y_pred = classifier.predict(x_test)
 
-    print('Decision tree classifier:')
-    print(confusion_matrix(y_test, y_pred))
+    print('Decision tree classifier')
     print(classification_report(y_test, y_pred))
+    plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='Decision tree classifier')
 
 
 def k_nearest_neighbors(json_data):
@@ -64,6 +67,6 @@ def k_nearest_neighbors(json_data):
 
     y_pred = classifier.predict(x_test)
 
-    print('K-nearest neighbors classifier:')
-    print(confusion_matrix(y_test, y_pred))
+    print('K-nearest neighbors classifier')
     print(classification_report(y_test, y_pred))
+    plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='K-nearest neighbors classifier')
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import json
+
+import matplotlib.pyplot as plt
+
+
 from classifiers import decision_tree_classifier, svm_classifier, k_nearest_neighbors
 
 if __name__ == "__main__":
@@ -7,4 +11,6 @@ if __name__ == "__main__":
 
     decision_tree_classifier(json_data=json_data)
     k_nearest_neighbors(json_data=json_data)
-    svm_classifier(json_data=json_data)
+    # svm_classifier(json_data=json_data)
+
+    plt.show()
