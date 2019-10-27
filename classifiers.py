bb4f3cbdb802dec3ed1f27f7a5eba6e4bc689726
Ivan Prsa <prsa.ivan13@gmail.com>
2019-10-27 23:00:53 +0100

    working on some more classifiers and ploting results

--- classifiers.py
+++ classifiers.py
@@ -4,15 +4,64 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn import linear_model
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
+
 from helpers import plot_confusion_matrix
 
 
+def linear_svc_classifier(json_data):
+
+    df = pd.DataFrame(data=json_data)
+
+    x = df.drop('type', axis=1)
+    y = df['type']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05, random_state=0)
+
+    lsvcc = LinearSVC(C=0.01, tol=1e-5, max_iter=2500)
+
+    lsvcc.fit(x_train, y_train)
+
+    y_pred = lsvcc.predict(x_test)
+
+    print('Linear SVC classifier:')
+    print(classification_report(y_test, y_pred))
+    plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='Linear SVM classifier')
+
+
+def sgd_classifier(json_data):
+
+    df = pd.DataFrame(data=json_data)
+
+    x = df.drop('type', axis=1)
+    y = df['type']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05, random_state=0)
+
+    sgdc = SGDClassifier(alpha=0.0001, average=False, class_weight=None,
+           early_stopping=False, epsilon=0.1, eta0=0.0, fit_intercept=True,
+           l1_ratio=0.15, learning_rate='optimal', loss='hinge', max_iter=2500,
+           n_iter_no_change=5, n_jobs=None, penalty='l2', power_t=0.5,
+           random_state=None, shuffle=True, tol=0.001,
+           validation_fraction=0.1, verbose=0, warm_start=False)
+
+    sgdc.fit(x_train, y_train)
+
+    y_pred = sgdc.predict(x_test)
+
+    print('SGD Classifier')
+    print(classification_report(y_test, y_pred))
+    plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='SGD Classifier')
+
+
 def svm_classifier(json_data):
-    sv_class = SVC(kernel='poly', degree=2) # linear/poly/rbf/sigmoid
+    sv_class = SVC(gamma='scale', decision_function_shape='ovo')
 
     df = pd.DataFrame(data=json_data)
 
@@ -25,7 +74,25 @@ def svm_classifier(json_data):
 
     y_pred = sv_class.predict(x_test)
 
-    print('SVM classifier')
+    print('SVM classifier:')
+    print(classification_report(y_test, y_pred))
+    plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='SVM classifier')
+
+
+def lasso(json_data):
+    reg = linear_model.Lasso(alpha=0.1)
+    df = pd.DataFrame(data=json_data)
+
+    x = df.drop('type', axis=1)
+    y = df['type']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05, random_state=0)
+
+    reg.fit(x_train, y_train)
+
+    y_pred = reg.predict(x_test)
+
+    print('Lasso classifier:')
     print(classification_report(y_test, y_pred))
     plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='SVM classifier')
 
@@ -43,7 +110,7 @@ def decision_tree_classifier(json_data):
 
     y_pred = classifier.predict(x_test)
 
-    print('Decision tree classifier')
+    print('Decision tree classifier:')
     print(classification_report(y_test, y_pred))
     plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='Decision tree classifier')
 
@@ -67,6 +134,6 @@ def k_nearest_neighbors(json_data):
 
     y_pred = classifier.predict(x_test)
 
-    print('K-nearest neighbors classifier')
+    print('K-nearest neighbors classifier:')
     print(classification_report(y_test, y_pred))
     plot_confusion_matrix(y_true=y_test, y_pred=y_pred, classes=y.unique(), title='K-nearest neighbors classifier')
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import json
 import matplotlib.pyplot as plt
 
 
-from classifiers import decision_tree_classifier, svm_classifier, k_nearest_neighbors
+from classifiers import decision_tree_classifier, lasso, sgd_classifier, linear_svc_classifier, svm_classifier, k_nearest_neighbors
 
 if __name__ == "__main__":
     with open('data/sampledata.json') as data:
         json_data = [json.loads(line) for line in data]
 
-    decision_tree_classifier(json_data=json_data)
-    k_nearest_neighbors(json_data=json_data)
+    # decision_tree_classifier(json_data=json_data)
+    # k_nearest_neighbors(json_data=json_data)
     # svm_classifier(json_data=json_data)
+    # lasso(json_data=json_data)
+    # linear_svc_classifier(json_data=json_data)
+    sgd_classifier(json_data=json_data)
 
     plt.show()
